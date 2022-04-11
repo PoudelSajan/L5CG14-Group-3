720e83d66e0570c749cde6e2d31d93f75de69d50
PoudelSajan <101956943+PoudelSajan@users.noreply.github.com>
2022-04-11 17:00:55 +0545

    Sajan Poudel

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!-- Contact -->
+<form>
+<br />
+<h2>Contact us</h2>
+<br />
+<p><label>First Name:</label> <input name="myEmail" type="text" /></p>
+<p><label>Email Address:</label> <input style="cursor: pointer;" name="myEmail" type="text" /></p>
+<p><label>Message:</label>  <textarea name="message"></textarea> </p>
+<p><input type="submit" value="Send" /></p>
+</form>
+ <!-- Contact Map Content -->
+ <div class="contact_map_content">
+        <div class="d-flex flex-column align-items-center justify-content-center">
+          <img class="contact_info_logo_1" src="images/logo_4.png" alt="">
+          
+          <div class="contact_info_list">
+            <ul class="text-center">
+              <li>Ghattekulo, Kathmandu, Nepal</li>
+              <li>Poudelsajan403@gmail.cpm</li>
+              <li>9861007426</li>
+             
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
---
+++ home.php
@@ -0,0 +1,275 @@
+<div class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Namaste</div>
+            <h1>Amazing tour packages in front of you.</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row intro_row">
+        <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1">
+          <div class="intro_text text-center">
+            <p>OLOC Travel special private tours designed for making the tour of the clients more special and memorable who have short limited time in Nepal. There are similar to the day tour packages but consists of special activities during the tour. The food, accommodation will be at special and luxurious places in Nepal.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row gallery_row">
+        <div class="col">
+
+          <!-- Gallery -->
+          <div class="gallery_slider_container">
+            <div class="owl-carousel owl-theme gallery_slider">
+              
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/h_1.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/h_2.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/h_3.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/h_4.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_right container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6 order-xl-1 order-2">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/tour_1.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/tour_2.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/tour_3.jpg)"></div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <!-- 
+
+  <!-- Rooms -->
+
+  <div class="rooms_left container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+        
+        <!-- Rooms Content -->
+        <div class="col-xl-6">
+          <div class="rooms_left_content">
+            <div class="section_title">
+              <div>Places</div>
+              <h1>Domestic Packages</h1>
+            </div>
+            <div class="rooms_text">
+            <p>Explore Nepal over the course of six days on this private tour. Planning a multi-day excursion like this one may be logistically challenging; booking this package means everything is arranged for you. You can just enjoy the sights, smells, and sounds of Nepal and listen to guided commentary as you explore.A private tour promises personalized attention, a flexible itinerary, and the chance to ask questions.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>All transportation in a comfortable private vehicle</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Overnight accommodations with breakfast each day</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>One local SIM card (returnable) for your phone and A bottle of mineral water each day to stay hydrated</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">Rs.50,000<span>Only</span></div>
+           
+          </div>
+        </div>
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/local_1.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/local_2.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/local_3.jpg)"></div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  
+    <!-- Discover Slider -->
+    <div class="discover_slider_container">
+      <div class="owl-carousel owl-theme discover_slider">
+        
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_1.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Travel</a></h1>
+          </div>
+        </div>
+
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_2.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Relax</a></h1>
+          </div>
+        </div>
+
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_3.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Enjoy</a></h1>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Testimonials -->
+
+  <div class="testimonials">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Clients</div>
+            <h1>Testimonials</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row testimonials_row">
+        <div class="col">
+          
+          <!-- Testimonials Slider -->
+          <div class="testimonials_slider_container">
+            <div class="owl-carousel owl-theme testimonials_slider">
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>Booking through you was very easy and made our lives so much easier. I have nothing bad to say! Thank you for giving us tips and guidance before we left on what to bring and such, that was very helpful! Thanks again.</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Maria Smith,</a><span> Client</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>Booking through you was very easy and made our lives so much easier. I have nothing bad to say! Thank you for giving us tips and guidance before we left on what to bring and such, that was very helpful! Thanks again.</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Krizan</a><span> Client</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>Wow Sajan. I thank you for all your effort and travel information. I really wasn’t expecting that you do this. So thank you very much.
+
+</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Sujan Shrestha,</a><span> Client</span></div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
